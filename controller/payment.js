f6ca39977df0d96c4d197611927ed2c60a5a4d6d
Muqtar Ajiboye <139395499+maoty321@users.noreply.github.com>
2025-09-24 13:25:57 +0100

    Update payment.js

--- controller/payment.js
+++ controller/payment.js
@@ -13,7 +13,7 @@ exports.payment = async(req, res) => {
     const price = req.session.price
     const response = await axios.post('https://api.paystack.co/transaction/initialize', {
       email: passenger.email[0],
-      callback_url:  "http://localhost:5000/siwesflight/verify-payment",
+      callback_url:  "https://siwesflight-ll5w.onrender.com/verify-payment",
       amount: amount * 100,
       metadata: {
         passenger: passenger,

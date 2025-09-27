71293c8dfe23767b80cf65e1de0e90ce022aa16f
maoty321 <muqtarajiboye>
2025-09-27 16:51:36 +0500

    add some package

--- controller/book-ticket.js
+++ controller/book-ticket.js
@@ -1,5 +1,6 @@
 const Booking = require("../model/book-ticket")
 const Flight = require('../model/flight');
+const Fares = require('../model/fares');
 
 exports.get_bookedtickets = async(req, res) => {
     const get_bookedtickets = await Booking.find({})
@@ -20,4 +21,18 @@ exports.get_bookedtickets = async(req, res) => {
 
     // res.json({tickets})
     res.render('admin/book--ticket', { alertMsg: false, tickets })
+}
+
+
+exports.get_bookedticket = async(req, res) => {
+    const {ticket_id } = req.params
+    const get_bookedticket = await Booking.findOne({"flight.reference": ticket_id })
+
+    const flight_number = get_bookedticket.flight.flight_number
+    const price_book = get_bookedticket.flight.price
+
+    const flight = await Flight.findOne({flight_number: flight_number })
+
+    // res.json({get_bookedticket, flight})
+    res.render('admin/view-ticket', { alertMsg: false, get_bookedticket, flight })
 }
--- route/flight.js
+++ route/flight.js
@@ -4,7 +4,7 @@ createFare, updateFare, deleteFare, dashboard,
 getFares,
 get_create_flight} = require("../controller/flight");
 const Airport = require("../model/airport");
-const { get_bookedtickets } = require("../controller/book-ticket");
+const { get_bookedtickets, get_bookedticket } = require("../controller/book-ticket");
 const flightRouter = express.Router();
 const authUser = require('../middleware/authmiddel')
 
@@ -14,6 +14,7 @@ flightRouter.get("/", authUser, get_create_flight)
 flightRouter.post("/", authUser, createFlight)
 flightRouter.get("/manage-flight", authUser, getFlights)
 flightRouter.get("/book-ticket", authUser, get_bookedtickets)
+flightRouter.get("/ticket/:ticket_id", authUser, get_bookedticket)
 
 
 flightRouter.get("/view/:id", authUser, getFlight)
